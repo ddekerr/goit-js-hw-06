1c08402d960fc87599bebd84d1c5d9f83071cf5a
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-06-15 23:38:35 +0300

    Second task is complete

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// array of future li
+const listOfIngredients = [];
+
+/*
+* create li for each ingredients
+* set them class "item"
+* set them text content is item of ingredients
+* add element to listOfIngredients array
+*/
+for (const item of ingredients) {
+  const itemOfIngredients = document.createElement("li");
+  itemOfIngredients.className = "item";
+  itemOfIngredients.textContent = item;
+  listOfIngredients.push(itemOfIngredients);
+}
+
+// Add all element like a child of ul#ingredients
+document.querySelector("#ingredients").append(...listOfIngredients);
