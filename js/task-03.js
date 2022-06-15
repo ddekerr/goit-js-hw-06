036d05a63eb75e85bfc9036c2448044530b26787
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-06-16 00:55:07 +0300

    Third task is complete

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+for (const elementKey of Object.keys(images)) {
+  // create item of list with an inner image for each of images array
+  const item = `<li class="item"><img src="${images[elementKey].url}" alt="${images[elementKey].alt}"></li>`;
+  // insert each element to the ul.gallery
+  document.querySelector(".gallery").insertAdjacentHTML("beforeend", item);
+}  
