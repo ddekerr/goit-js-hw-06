a8a717b45e92e4f2650fc4b21adea5e7bf7c9e6b
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-06-17 23:03:18 +0300

    Ninth task complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Click event
+const changeColorBtn = document.querySelector(".change-color");
+changeColorBtn.addEventListener('click', function() {
+  // insert color value in colorResult element
+  const colorResult = document.querySelector('.color');
+  colorResult.textContent = getRandomHexColor();
+
+  // change bg-color of body
+  document.body.style.backgroundColor = getRandomHexColor();
+});
