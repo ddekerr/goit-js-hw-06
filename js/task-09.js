5f6b4e5a0b2caa0e9b70ed727f5f6b39a00f7991
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-06-21 00:33:09 +0300

    Ninth task bug fix

--- js/task-09.js
+++ js/task-09.js
@@ -7,8 +7,9 @@ const changeColorBtn = document.querySelector(".change-color");
 changeColorBtn.addEventListener('click', function() {
   // insert color value in colorResult element
   const colorResult = document.querySelector('.color');
-  colorResult.textContent = getRandomHexColor();
+  const color = getRandomHexColor();
+  colorResult.textContent = color;
 
   // change bg-color of body
-  document.body.style.backgroundColor = getRandomHexColor();
+  document.body.style.backgroundColor = color;
 });
