67b4ef338282956517a002f093a6da007721e2dd
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-08-24 23:55:55 +0300

    Refactor 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,42 +1,35 @@
-const counter = {  
-  value : 0,
-  refs: {},
+class Counter {  
+  #value = 0;
 
-  // Set all counter references to this.refs
-  setRefs(selector)  {
-    this.refs.container = document.querySelector(selector);
-    this.refs.incrementBtn = document.querySelector('[data-action="increment"]');
-    this.refs.decrementBtn = document.querySelector('[data-action="decrement"]');
-    this.refs.value = document.querySelector('#value');
-  },
-
-  // increase value to 1
   increment() {
-    this.value += 1;
-  },
+    this.#value += 1;
+  }
   
-  // decrease value to 1
   decrement() {
-    this.value -= 1;
-  },
+    this.#value -= 1;
+  }
 
-  // update content of refs.value
-  showValueInHtml() {
-    this.refs.value.textContent = this.value;
+  get value() {
+    return this.#value;
   }
 };
 
-// set object references
-counter.setRefs("#counter");
+const refs = {
+  incrementBtn: document.querySelector('[data-action="increment"]'),
+  decrementBtn: document.querySelector('[data-action="decrement"]'),
+  value: document.querySelector('#value')
+}
+
+const counter = new Counter();
 
 // event click for increment button
-counter.refs.incrementBtn.addEventListener('click', () => {
+refs.incrementBtn.addEventListener('click', () => {
   counter.increment();
-  counter.showValueInHtml();
+  refs.value.textContent = counter.value;
 });
 
 // event click for decrement button
-counter.refs.decrementBtn.addEventListener('click', () => {
+refs.decrementBtn.addEventListener('click', () => {
   counter.decrement();
-  counter.showValueInHtml();
+  refs.value.textContent = counter.value;
 });
