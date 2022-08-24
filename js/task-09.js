c7f2a8c36f9ec2e27a2c79ee6acc695592d25b0f
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-08-25 01:14:35 +0300

    Refactor 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,11 +2,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// Click event
 const changeColorBtn = document.querySelector(".change-color");
+const colorResult = document.querySelector('.color');
+
+// Click event
 changeColorBtn.addEventListener('click', function() {
   // insert color value in colorResult element
-  const colorResult = document.querySelector('.color');
   const color = getRandomHexColor();
   colorResult.textContent = color;
 
