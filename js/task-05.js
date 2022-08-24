7c5f6a2225530ec987aacbdd348f3877c7763f2e
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-08-25 00:06:25 +0300

    Refactor 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,32 +1,12 @@
-const substituter = {
-  inputValue: '',
-  refs: {},
-
-  // set all substituter references to this.refs
-  setRefs(...selectors) {
-    this.refs.inputElement = document.querySelector(selectors[0]);
-    this.refs.outputElement = document.querySelector(selectors[1]);
-  },  
-
-  // update inputValue from inputElement
-  updateInputValue() {
-    this.inputValue = this.refs.inputElement.value;
-  },
-
-  // show content of inputElement in OutputElement
-  showInputContent() {
-    if (this.inputValue === '')
-      this.refs.outputElement.innerHTML = 'Anonymous';
-    else
-      this.refs.outputElement.innerHTML = this.inputValue;
-  }
-}
-
-// set object references
-substituter.setRefs("#name-input", "#name-output");
+// set all substituter references to this.refs
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
 
 // input event for inputElement
-substituter.refs.inputElement.addEventListener('input', () => {
-  substituter.updateInputValue();
-  substituter.showInputContent();
+input.addEventListener('input', function(e) {
+  updateInputValue(e.target.value)
 });
+
+function updateInputValue(value) {
+  output.textContent = input.value === '' ? 'Anonymus' : value;
+}
