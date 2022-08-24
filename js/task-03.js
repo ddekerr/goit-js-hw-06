ed9522bc077d49bb26b689b2c20d47704b5e16db
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-08-24 23:47:35 +0300

    Refactor 3

--- js/task-03.js
+++ js/task-03.js
@@ -13,9 +13,10 @@ const images = [
   },
 ];
 
-for (const elementKey of Object.keys(images)) {
-  // create item of list with an inner image for each of images array
-  const item = `<li class="item"><img src="${images[elementKey].url}" alt="${images[elementKey].alt}"></li>`;
-  // insert each element to the ul.gallery
-  document.querySelector(".gallery").insertAdjacentHTML("beforeend", item);
-}  
+const markup = images.map(image => {
+  return `<li class="item">
+            <img src="${image.url}" alt="${image.alt}">
+          </li>`
+}).join('');
+document.querySelector(".gallery").innerHTML = markup;
+
